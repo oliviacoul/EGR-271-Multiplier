893ad091a2b25b30879f44c69373397d9991c7ce
Julio Archila <jarchila929@gmail.com>
2024-11-19 18:18:38 -0500

    Initial code

--- TOP.vhd
+++ TOP.vhd
@@ -17,3 +17,28 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TOP is
+    Port ( reset : in STD_LOGIC;
+           input : in STD_LOGIC;
+           calculate : in STD_LOGIC);
+end TOP;
+
+architecture Behavioral of TOP is
+
+begin
+
+
+end Behavioral;
